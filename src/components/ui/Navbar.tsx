59ed1ecfc104d3dc7769d8cd18570db366db8230
mibrgmv <mibrgmv@gmail.com>
2025-03-02 15:18:26 +0300

    refac: navbar styles to .css

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link as RouterLink } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext.tsx';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -9,52 +10,52 @@ const Navbar = () => {
     const { user, logout } = useAuth();
 
     return (
-        <div className='absolute top-0 min-h-20 w-full flex justify-between items-center text-sm text-white bg-black'>
-            <div>
-                <span className='text-3xl my-5 ms-5 md:ms-[5vw]'>.testapp</span>
+        <div className={styles.navbar}>
+            <div className={styles.logo}>
+                .testapp
             </div>
 
-            <ul className='hidden md:flex justify-center list-none'>
-                <li className='my-5 me-[5vw] no-underline cursor-pointer hover:underline'>
+            <ul className={styles.navLinks}>
+                <li>
                     <RouterLink to='/'>Home</RouterLink>
                 </li>
-                <li className='my-5 me-[5vw] no-underline cursor-pointer hover:underline'>
+                <li>
                     <RouterLink to='/users'>Users</RouterLink>
                 </li>
-                <li className='my-5 me-[5vw] no-underline cursor-pointer hover:underline'>
+                <li>
                     <RouterLink to='/profile'>Profile</RouterLink>
                 </li>
                 {user ? (
-                    <li className='my-5 me-[5vw] no-underline cursor-pointer hover:underline'>
+                    <li>
                         <button className='inline' onClick={logout}>Logout</button>
                     </li>
                 ) : (
-                    <li className='my-5 me-[5vw] no-underline cursor-pointer hover:underline'>
+                    <li>
                         <RouterLink to='/login'>Login</RouterLink>
                     </li>
                 )}
             </ul>
 
-            <div onClick={handleClick} className='md:hidden z-10 cursor-pointer my-5 me-5'>
+            <div onClick={handleClick} className={styles.mobileIcon}>
                 {!nav ? <FaBars /> : <FaTimes />}
             </div>
 
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black text-white flex flex-col justify-center items-center'}>
-                <li className='my-6 text-4xl cursor-pointer hover:underline'>
+            <ul className={!nav ? styles.hidden : styles.navLinksMobile}>
+                <li>
                     <RouterLink onClick={handleClick} to='/'>Home</RouterLink>
                 </li>
-                <li className='my-6 text-4xl cursor-pointer hover:underline'>
+                <li>
                     <RouterLink onClick={handleClick} to='/users'>Users</RouterLink>
                 </li>
-                <li className='my-6 text-4xl cursor-pointer hover:underline'>
+                <li>
                     <RouterLink onClick={handleClick} to='/profile'>Profile</RouterLink>
                 </li>
                 {user ? (
-                    <li className='my-6 text-4xl cursor-pointer hover:underline'>
+                    <li>
                         <button onClick={logout}>Logout</button>
                     </li>
                 ) : (
-                    <li className='my-6 text-4xl cursor-pointer hover:underline'>
+                    <li>
                         <RouterLink onClick={handleClick} to='/login'>Login</RouterLink>
                     </li>
                 )}
