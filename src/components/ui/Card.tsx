2c581af16219f1a0946bf6106adbac6b7e609d1f
mibrgmv <mibrgmv@gmail.com>
2025-03-15 20:43:27 +0300

    fix: card button inconsistency

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 export const Card: React.FC<Props> = ({name, buttonText, onClick}) => {
     return (
-        <div className="p-6 rounded-lg shadow-md cursor-pointer hover:bg-gray-100">
-            <h3 className="text-xl font-bold">{name}</h3>
+        <div className="flex flex-col p-6 rounded-lg shadow-md cursor-pointer hover:bg-gray-100">
+            <h3 className="flex-1 text-xl font-bold">{name}</h3>
             <div className="mt-4">
                 <Button text={buttonText} onClick={onClick}/>
             </div>
