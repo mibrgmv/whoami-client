e2523cde3dd952e92a74a5e1dc275d1849a03e7e
mibrgmv <mibrgmv@gmail.com>
2025-04-26 02:34:21 +0300

    feat: quizzes icon

--- src/components/ui/navbar/Logo.tsx
+++ src/components/ui/navbar/Logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = () => {
     return (
-        <RouterLink to="/" className="fixed p-3 top-3 left-5 text-3xl rounded-full">
+        <RouterLink to="/" className="fixed py-2 px-4 top-3 left-5 bg-white text-3xl rounded-full">
             ./whoami
         </RouterLink>
     );
